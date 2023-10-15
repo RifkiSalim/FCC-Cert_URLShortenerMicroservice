1b69baaadc3de5818625b4b0cbe773fa1230dbd5
Rifki Salim <contact@rifkisalim.com>
2023-10-15 14:05:56 +0530

    🦄 refactor(index): refactor routes

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+// Imports
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+// Routers
+const apiRouter = require("./routes/api");
+
+// Init app
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({ optionsSuccessStatus: 200 }));
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
+// Config Routers
+app.use("/api", apiRouter);
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
